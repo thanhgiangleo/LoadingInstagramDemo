4d5d7ffae90c8279bd6b9867bbf341c1e78c2ace
donaldtrung <nhtrung13clc@gmail.com>
2018-01-13 09:16:06 +0700

    demo INS

---
+++ InstagramDemo/app/Http/Controllers/InstagramController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class InstagramController extends Controller
+{
+   public function index()
+   {
+       $json = file_get_contents('https://api.instagram.com/v1/users/self/media/recent/?access_token=2207028252.6dd3f5d.8ee1b837794843389924fb27561c3fb2');
+       $obj = json_decode($json);
+
+       return view('ins', ['data' => $obj->data]);
+   }
+}
---
+++ InstagramDemo/public/js/main.js
@@ -0,0 +1,148 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+
+	var burgerMenu = function() {
+
+		$('.js-fh5co-nav-toggle').on('click', function(event) {
+			event.preventDefault();
+			var $this = $(this);
+			if( $('body').hasClass('menu-show') ) {
+				$('body').removeClass('menu-show');
+				$('#fh5co-main-nav > .js-fh5co-nav-toggle').removeClass('show');
+			} else {
+				$('body').addClass('menu-show');
+				setTimeout(function(){
+					$('#fh5co-main-nav > .js-fh5co-nav-toggle').addClass('show');
+				}, 900);
+			}
+		})
+	};
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn animated');
+							} else {
+								el.addClass('fadeInUp animated');
+							}
+
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+	
+	var counter = function() {
+		$('.js-counter').countTo({
+			formatter: function (value, options) {
+	      	return value.toFixed(options.decimals);
+	    	}
+		});
+	};
+
+	var counterWayPoint = function() {
+		if ($('#counter-animate').length > 0 ) {
+			$('#counter-animate').waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+					setTimeout( counter , 400);					
+					$(this.element).addClass('animated');
+						
+				}
+			} , { offset: '90%' } );
+		}
+	};
+
+
+	var imgPopup = function() {
+
+
+		$('body').on('click', '.img-popup', function(event){
+			event.preventDefault();
+			var src = $(this).attr('href');
+			var link = $(this).attr('data-link');
+
+			var html = '';
+        	html += '<div class="mfp-figure"><button title="Close (Esc)" type="button" class="mfp-close">×</button><figure>';
+        	html += '<img class="mfp-img" src=' + src + ' data-link=' + link + ' style="max-height: 576px;"><figcaption>';
+        	html += '<div class="mfp-bottom-bar"><div class="mfp-title"></div><div class="mfp-counter"></div></div></figcaption></figure></div>';
+
+			$.magnificPopup.open({
+		      items: {
+                  src: html,
+				  type: 'inline'
+			   },
+			   type: 'image'
+		  	});
+				
+		});
+
+        $('body').on('click', '.mfp-img', function(event){
+            var url = $(this).attr('data-link');
+            openInNewTab(url);
+        });
+	};
+
+    var openInNewTab = function(url) {
+        var win = window.open(url, '_blank');
+        win.focus();
+    }
+
+
+	// Document on load.
+	$(function(){
+		fullHeight();
+		burgerMenu();
+		counter();
+		contentWayPoint();
+counterWayPoint();
+		imgPopup();
+	});
+
+
+}());
