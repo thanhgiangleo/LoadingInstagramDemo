ec92ebcb246cd21fa82926f0e4ee6f0fe39394d6
Dinh Trang Thanh Giang <thanhgiang.hcmus@gmail.com>
2018-01-25 15:12:35 +0700

    layout v4 - post detail

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,12 +10,8 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="{{ asset("css/default.css") }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset("css/fonts.css") }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset("css/post-details.css") }}" rel="stylesheet" type="text/css" media="all" />
     <link href="{{ asset("css/stylef2b5.css") }}" rel="stylesheet" type="text/css" media="all" />
-
-    {{--<link rel="stylesheet" type="text/css" href="slick/slick.css"/>--}}
-    {{--<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>--}}
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
